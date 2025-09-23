93da745cc361b60ba2fbe531f3a1b39056902c0e
akarsh363 <122525163+akarsh363@users.noreply.github.com>
2025-09-23 20:08:21 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//    <p>Hiiiii</p>
-//     </>
-//   )
-// }
-
-// export default App
-
-
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
